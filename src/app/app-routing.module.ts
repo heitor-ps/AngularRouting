1a3f93f71147e67225fd84b3cf93258e947cc65e
Heitor Soares <heytorpg7@gmail.com>
2022-10-16 17:44:34 -0300

    created login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'pagina-com-parametros/:id',component: PaginaComParametrosComponent},
   { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
   { path: 'pagina-protegida', component: PaginaProtegidaComponent, canActivate: [AuthGuard]},
+  {path :'login', component: LoginComponent},
   { path: '**', component: PaginaNaoEncontradaComponent}
 ]
 
