af7aa5424a33e035084ba8f2b50ed5220730c8de
Heitor Soares <heytorpg7@gmail.com>
2022-10-16 17:33:04 -0300

    pagina-protegida component route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'primeira-pagina', pathMatch: 'full'},
   { path: 'pagina-com-parametros/:id',component: PaginaComParametrosComponent},
   { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: 'pagina-protegida', component: PaginaProtegidaComponent},
   { path: '**', component: PaginaNaoEncontradaComponent}
 ]
 
