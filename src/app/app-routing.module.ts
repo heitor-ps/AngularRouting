a6eb10ea9f6652179244faeb21b7db2af5ee9745
Heitor Soares <heytorpg7@gmail.com>
2022-10-16 17:37:25 -0300

    added authguard guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
@@ -13,7 +14,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'primeira-pagina', pathMatch: 'full'},
   { path: 'pagina-com-parametros/:id',component: PaginaComParametrosComponent},
   { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
-  { path: 'pagina-protegida', component: PaginaProtegidaComponent},
+  { path: 'pagina-protegida', component: PaginaProtegidaComponent, canActivate: [AuthGuard]},
   { path: '**', component: PaginaNaoEncontradaComponent}
 ]
 
